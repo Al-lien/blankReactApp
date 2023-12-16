215fbde0c415e6b648345d930b34cd326556e869
Alexis_Lienard <lienard.alexis@gmail.com>
2023-12-16 15:54:11 +0100

    + react-router-dom(frontend)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// pages & components
+import Home from "./pages/Home";
+
+// styles
+import "./App.css";
 function App() {
   return (
     <>
-      <h1>How to change remote git repo</h1>
-      <br />
-      <p>$ git remote add origin NEW_REMOTE_URL</p>
-      <p>$ git remote -v</p>
-      <br />
-      <br />
-      <h2>How to change project name</h2>
-      <br />
-      <p>Change name in package.json(1) and package-lock.json(2)</p>
-      <br />
-      <hr />
-      <br />
-      <small>more info in README.md file</small>
+      <div className="App">
+        <BrowserRouter>
+          <div className="pages">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
     </>
   );
 }

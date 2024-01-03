db41155a69e0af80394b0c7f18500316a5fb202c
Alexis_Lienard <lienard.alexis@gmail.com>
2024-01-03 14:32:15 +0100

    - deleted ./backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,30 +1,9 @@
-import { useEffect, useState } from "react";
-
 function Home() {
-
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch("http://localhost:3000/api/basicRoutes");
-
-            const json = await response.json();
-
-            if (response.ok) {
-                setData(json);
-            }
-
-        }
-        fetchData();
-    }, [])
-
-    { data && console.log(data) }
-
-    return (
-        <div className="home">
-            <h2>Home</h2>
-        </div>
-    );
+  return (
+    <div className="home">
+      <h2>Home</h2>
+    </div>
+  );
 }
 
 export default Home;

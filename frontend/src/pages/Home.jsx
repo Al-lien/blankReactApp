5e9cb081d207bc51461cf14fae99e68d475b7952
Alexis_Lienard <lienard.alexis@gmail.com>
2024-01-05 16:21:01 +0100

    + added npm run dev cmd from root dir to run both front & back  server

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,30 +1,30 @@
 import { useEffect, useState } from "react";
 
 function Home() {
+  const [data, setData] = useState(null);
 
-    const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3000/api/basicRoutes");
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch("http://localhost:3000/api/basicRoutes");
+      const json = await response.json();
 
-            const json = await response.json();
+      if (response.ok) {
+        setData(json);
+      }
+    };
+    fetchData();
+  }, []);
 
-            if (response.ok) {
-                setData(json);
-            }
+  {
+    data && console.log(data);
+  }
 
-        }
-        fetchData();
-    }, [])
-
-    { data && console.log(data) }
-
-    return (
-        <div className="home">
-            <h2>Home</h2>
-        </div>
-    );
+  return (
+    <div className="home">
+      <h2>Home</h2>
+    </div>
+  );
 }
 
 export default Home;

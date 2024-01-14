f20f71e767950f1dfed9a7f871b033139267458a
Alexis_Lienard <lienard.alexis@gmail.com>
2024-01-14 14:04:34 +0100

    + added fetch function to test connexion to db

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+
 function Home() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3000/api/basicRoutes");
+
+      const json = await response.json();
+
+      if (response.ok) {
+        setData(json);
+      }
+    };
+    fetchData();
+  }, []);
+
+  {
+    data && console.log(data);
+  }
+
   return (
     <div className="home">
       <h2>Home</h2>

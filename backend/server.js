5197129f8c3597a616c793700d751535494a22e9
Alexis_Lienard <lienard.alexis@gmail.com>
2024-01-14 13:54:44 +0100

    #up-to-date with main branch changes (husky, linter & prettier)

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+// 👇 import basic routes patern
+const basicRoutes = require("./routes/basicRoutes");
+
+// 👇 express app
+const app = express();
+
+// 👇 middleware
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+/* parses the JSON data and makes it available in req.body */
+app.use(express.json());
+/* console.log path and request type  */
+app.use((req, res, next) => {
+  console.info(req.path, req.method);
+  next();
+});
+
+// 👇 routes
+app.use("/api/basicRoutes", basicRoutes);
+
+// 👇 listen for request
+app.listen(process.env.PORT, () => {
+  console.info("Listening on port", process.env.PORT);
+});
